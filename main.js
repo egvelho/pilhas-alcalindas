66de23eb90a8c267ffd506ab7dc1e1ce8f6607ed
Eduarda Velho <email@eduardavelho.com>
2025-09-25 20:20:57 -0300

    init

---
+++ main.js
@@ -0,0 +1,9 @@
+import { Stack } from "./stack/stack.js";
+
+const stack = new Stack();
+stack.push("A");
+stack.push("B");
+stack.push("C");
+const c = stack.pop();
+console.log(c);
+stack.print();
---
+++ stack/stack.js
@@ -0,0 +1,18 @@
+export class Stack {
+  constructor() {
+    this.top = null;
+    this.size = 0;
+  }
+
+  // coloca um novo elemento no topo da pilha
+  push(value) {}
+
+  // remove o elemento do topo da pilha, retornando-o
+  pop() {}
+
+  // retorna o elemento do topo da pilha, sem alterar nada
+  peek() {}
+
+  // mostra a pilha do topo até o fundo
+  print() {}
+}
